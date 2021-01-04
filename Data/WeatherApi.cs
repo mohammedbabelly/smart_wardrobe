7c3c62eda2b97dd822718450db0c5dda38538d4e
mohammedbabelly <mohammedababelly@gmail.com>
2021-01-04 14:58:50 +0200

    Add project files.

---
+++ Data/WeatherApi.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace smart_wardrobe.Data {
+    public class WeatherAPI {
+
+        private const string APIKEY = "80c98f42629f6d9a7dcd6b6c38f9cd6b";
+        private string _weatherURL;
+        private string _baseURL = "https://api.openweathermap.org";
+        public WeatherAPI(string city) {
+            SetCurrentURL(city);
+        }
+
+        private void SetCurrentURL(string city) {
+            _weatherURL = _baseURL + "/data/2.5/weather?q="
+                + city + "&appid=" + APIKEY + "&units=metric";
+        }
+        public string GetImgUrl(string code) => "http://openweathermap.org/img/wn/" + code + "@2x.png";
+
+        public Weather FetchData() {
+            using (var webClient = new WebClient()) {
+                var res = webClient.DownloadString(_weatherURL);
+                var weather =
+                 JsonConvert.DeserializeObject<Weather>(res);
+                return weather;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Prolog;
+using smart_wardrobe.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace smart_wardrobe {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            Console.WriteLine("test");
+            GetWeather();
+            ExcuteProlog();
+        }
+
+        public void ExcuteProlog() {
+            var prolog = new PrologEngine(persistentCommandHistory: false);
+            var query = "findOutfits(15,male,clear_day,Outfits).";
+            var solution2 = prolog.GetAllSolutions(AppDomain.CurrentDomain.BaseDirectory + "/Assets/prolog/outfits.pl", query);
+            foreach (var d in solution2.NextSolution) {
+                foreach (var s in d.NextVariable) {
+                    Console.WriteLine(s.Value);
+                }
+            }
+        }
+        public Weather GetWeather() {
+            Weather weather = new WeatherAPI("Aleppo").FetchData();
+            Console.WriteLine(weather.main.temp);
+            Console.WriteLine(weather.weather.First().description);
+            return weather;
+        }
+    }
+}
